e17f0844ae4f284274de96a620729888de49ea5a
diegok23 <dkogut@gmail.com>
2022-05-15 14:37:30 +0200

    Terminado

--- Rick and Morty/scripts/script.js
+++ Rick and Morty/scripts/script.js
@@ -11,8 +11,10 @@ function buildRoot() {
     '<div id="mainContent"></div>' +
     '</div>' +
     '</div>';
+  const mainContent = document.querySelector('#mainContent');
+  mainContent.innerHTML =
+    '<div class="titleImg"><img src="https://www.cinemascomics.com/wp-content/uploads/2021/12/rick-y-morty-navidad-video-rap.jpg" /></div>';
 }
-const episodesData = [];
 
 function loadEpisodes(url) {
   fetch(url || 'https://rickandmortyapi.com/api/episode')
@@ -22,7 +24,6 @@ function loadEpisodes(url) {
 }
 
 function showEpisodeLinks(episodes) {
-  episodesData.push(...episodes.results);
   const episodesContainer = document.querySelector('#leftContent');
   episodes.results.map((episode) => createEpisodeLink(episode)).forEach((node) => episodesContainer.appendChild(node));
   const loadMore = document.querySelector('#loadMore');
@@ -70,10 +71,49 @@ function showCharacterDetail(character) {
     `<div><div class='characterCardDetailHeader'>
       <div><img src=${character.image}></div>
       <div><h2>${character.name}</h2>` +
-    `<h3>${character.status} | ${character.species} | ${character.gender} | ${character.origin.name} </h3></div></div>
+    `<h3>${character.status} | ${character.species} | ${character.gender} | ${character.origin.name} </h3>
+    <div class='locationButton'><button id='locationButton'>Get Location</button></div></div></div>
       <div class='characterCardDetail'></div>
   </div> `;
-  character.episode.map((episode) => createEpisodeData(episode)).forEach((node) => mainContent.appendChild(node));
+  const locationButton = document.querySelector('#locationButton');
+  locationButton.addEventListener('click', () => renderLocation(character.origin.url));
+  console.log(character.origin.name);
+
+  character.episode.forEach((episode) => {
+    fetch(episode)
+      .then((res) => res.json())
+      .then((episodes) => {
+        const characterEpisodes = document.querySelector('.characterCardDetail');
+        const characterEpisodesDetail = document.createElement('div');
+        characterEpisodesDetail.className = 'characterEpisodesDetail';
+        characterEpisodes.appendChild(characterEpisodesDetail);
+        characterEpisodesDetail.innerHTML = `<div class='episodeNumber'><h3>Episode - ${episodes.id}</h3><h4 class="card-title">${episodes.episode}</h4></div>`;
+        characterEpisodesDetail.onclick = () => showEpisodeDetail(episodes);
+      });
+  });
+}
+
+function renderLocation(origin) {
+  fetch(origin)
+    .then((res) => res.json())
+    .then((origin) => {
+      const characterCardDetailHeader = document.querySelector('.characterCardDetailHeader');
+      characterCardDetailHeader.innerHTML = '<div><div>' + `<h1>${origin.name}</h1><h3>${origin.type} - ${origin.dimension}</h3>` + '</div>';
+      const characterCard = document.querySelector('.characterCardDetail');
+      characterCard.innerHTML = '';
+      origin.residents.forEach((resident) => {
+        fetch(resident)
+          .then((res) => res.json())
+          .then((character) => {
+            const episodeCard = document.createElement('div');
+            episodeCard.innerHTML = `<img src="${character.image}"><h4>${character.name}</h4><h5>${character.species} | ${character.status}</h5></div></div>`;
+            episodeCard.className = 'characterCard';
+            console.log(character.url);
+            episodeCard.onclick = () => showCharacterDetail(character);
+            characterCard.appendChild(episodeCard);
+          });
+      });
+    });
 }
 
 function createEpisodeData(episode) {
